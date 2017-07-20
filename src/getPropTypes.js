48afa81f96bafca4c9f65ebb5350c6cd1324b6d2
Ujjwal Mishra <ujjwalmishra09@gmail.com>
2017-07-21 00:37:07 +0530

    Create object from jest proptypes

--- src/getPropTypes.js
+++ src/getPropTypes.js
@@ -3,7 +3,25 @@ function getPropTypes(fileData, miningPos) {
   let string = '';
 
   for (let i = miningPos, count = 0; i < fileData.length; i += 1) {
-    string += fileData[i];
+    if (fileData.charCodeAt(i) !== 10 && fileData.charCodeAt(i) !== 32) {
+      if (
+        count === 1
+        &&
+        (fileData[i] === '}' || fileData[i] === ':' || fileData[i] === ',')
+      ) {
+        string += '"';
+      }
+
+      string += fileData[i];
+
+      if (
+        (count === 0 && fileData[i] === '{')
+        ||
+        (count === 1 && (fileData[i] === ':' || fileData[i] === ','))
+      ) {
+        string += '"';
+      }
+    }
 
     if (fileData[i] === '{') {
       count += 1;
@@ -12,7 +30,7 @@ function getPropTypes(fileData, miningPos) {
       if (count === 0) break;
     }
   }
-  return string;
+  return JSON.parse(string);
 }
 
 export default getPropTypes;
--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ const data = fs.readFileSync('./test.js', { encoding: 'utf8' });
 
 if (data) {
   const position = data.indexOf('propTypes =');
-  if (position > 0) getPropTypes(data, position);
+  // to check the response using console.log
+  if (position > 0) console.log(getPropTypes(data, position + 'propTypes ='.length)); // eslint-disable-line
 }
